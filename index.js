84cb3dc026e543a524852e2092e54ef78e54029f
Thangnt <thangnt336@gmail.com>
2021-06-21 16:09:16 -0400

    added menu

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const password = require('./password');
 const { printTable } = require('console-table-printer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: password,
-    database: 'employee_db',
+    password: '',
+    database: 'employee_DB',
 });
 
 connection.connect((err) => {
     if (err) throw err;
-    logo();
 });
+
+start();
+
+// Title screen
+function start() {
+    console.log('\n\nEmployee Management System\n\n');
+
+    inquirer
+        .prompt({
+            name: 'option',
+            type: 'list',
+            message: 'What would you like to access?',
+            choices: ['Employees', 'Departments', 'Roles', 'Exit'],
+        })
+        // .then((response) => {
+        //     // Options
+        //     switch (response.option) {
+        //         case 'Employees':
+        //             employees();
+        //             break;
+        //         case 'Departments':
+        //             departments();
+        //             break;
+        //         case 'Roles':
+        //             roles();
+        //             break;
+        //         case 'Exit':
+        //             log('Goodbye');
+        //             connection.end();
+        //             break;
+        //         default:
+        //             console.log('Error');
+        //             start();
+        //             break;
+        //     }
+        // });
+}
+
 
