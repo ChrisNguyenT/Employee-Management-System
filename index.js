d998c845bf27370327f39d5e94e911b61461bb6b
Thangnt <thangnt336@gmail.com>
2021-06-21 18:52:18 -0400

    added env

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const { printTable } = require('console-table-printer');
+const env = require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: process.env.password,
     database: 'employee_DB',
 });
 
@@ -27,28 +28,31 @@ function start() {
             message: 'What would you like to access?',
             choices: ['Employees', 'Departments', 'Roles', 'Exit'],
         })
-        // .then((response) => {
-        //     // Options
-        //     switch (response.option) {
-        //         case 'Employees':
-        //             employees();
-        //             break;
-        //         case 'Departments':
-        //             departments();
-        //             break;
-        //         case 'Roles':
-        //             roles();
-        //             break;
-        //         case 'Exit':
-        //             log('Goodbye');
-        //             connection.end();
-        //             break;
-        //         default:
-        //             console.log('Error');
-        //             start();
-        //             break;
-        //     }
-        // });
+        .then((response) => {
+            // Options
+            switch (response.option) {
+                case 'Employees':
+                    employees();
+                    break;
+                case 'Departments':
+                    departments();
+                    break;
+                case 'Roles':
+                    roles();
+                    break;
+                case 'Exit':
+                    log('Goodbye');
+                    connection.end();
+                    break;
+                default:
+                    console.log('Error');
+                    start();
+                    break;
+            }
+        });
 }
 
+function employees(){
+    return console.log('None');
+}
 
